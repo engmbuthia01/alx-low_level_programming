b6ae91738caf08c5c102d579330fc801ceee86a9
engmbuthia01 <kevinmbuthia3354@gmail.com>
2023-12-20 10:05:56 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetical character
+ * @c: character to be checked
+ * Return: 1 for lowercase or uppercase, 0 for otherwise
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
