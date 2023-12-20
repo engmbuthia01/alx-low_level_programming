f06dc9403f1f738811ba7eac4dde2c965857d39a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2023-12-20 11:49:48 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute from 00:00 to 23:59
+ * jack_bauer - prints every minute from
  * Return: ...
  */
 
@@ -13,7 +13,7 @@ int a, b, c, d;
 
 for (a = 0; a <= 2; a++)
 {
-for (b = 0; a <= 9; b++)
+for (b = 0; b <= 9; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
