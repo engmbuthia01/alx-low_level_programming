d5f2fd9e257b179c5e2ba98443595fd5deb60203
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-01-22 15:50:02 -0800

    Very hard task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,32 @@
 #define DOG_H
 
 /**
- * struct dog - elements of the dog
- * @name: first element
- * @age: second element
- * @owner: third element
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Desription: long description
+ * Description: Longer description
  */
-
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
+}
