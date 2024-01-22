cea59f68b983a0b86effd43368e1f1dedf77fef8
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-01-22 13:01:35 -0800

    main.h and dog.h created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - elements of the dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ *
+ * Desription: long description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
