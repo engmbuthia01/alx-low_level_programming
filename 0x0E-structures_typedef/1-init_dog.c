057eedf33479af90993a74a35905cccbc290326a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-01-22 16:15:07 -0800

    Wholesome

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @age: age to be initialized
  * @owner: owner to be initialized
  */
-	 
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
@@ -17,3 +17,4 @@ d->name = name;
 d->age = age;
 d->owner = owner;
 }
+
