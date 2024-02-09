eb2010e2115cde91f295c8e64c55b371e239defe
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-02-09 05:32:05 -0800

    a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: pointer to the number
+ * _pow - function calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
  *
- * Return: 0 (sucess)
+ * Return: value of (base ^ power)
  */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num = 1;
+	unsigned int a;
 
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int i;
+	unsigned long int divisor, check;
+	char flag = 0;
 
-    for (i = 0; i < sizeof(unsigned long int) * 8; i++)
-    {
-        if (n & mask)
-     	printf("1");
-        else
-            printf("0");
-        mask >>= 1;
-    }
-    printf("\n");
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
+
