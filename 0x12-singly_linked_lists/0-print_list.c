4773bfa2dfa97aa206dc68d1ee0b7b3f3b2a58a3
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-03-23 03:09:11 -0700

    used check() to include the string characters

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t check;
 
 	count = 0;
 	while (h != NULL)
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
-		count++;
+		check++;
 	}
 
-	return (count);
+	return (check);
 }
