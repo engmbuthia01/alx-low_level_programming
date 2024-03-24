7ec9735a860f9652dd44f3d01a4032901b8f4e45
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-03-24 01:14:19 -0700

    Tis is a unction that adds a new node at the end of a listint_t list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,30 +8,26 @@
  * Return: address of the new element, NULL if it fails
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
-	listint_int *last_node = *head;
+	listint_t *arrow = *head;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
 		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (*head == NULL)
+	if (arrow != NULL)
 	{
-		*head == new_node;
-		return (new_node);
+		while (arrow->next != NULL)
+			arrow = arrow->next;
+		arrow->next = new_node;
 	}
-
-	while (last_node->next != NULL)
-	{
-		last_node = last_node->next;
-	}
-
-	last_node->next = new_node;
-
+	else
+		*head = new_node;
 	return (new_node);
 }
