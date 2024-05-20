ba0ca2c51445500b864e399c7c3a77f80f68ee3b
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-19 22:56:34 -0700

    a function that prints a string before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * first -defines a function that is executed first,
+ * before the main function.
+ *
+ * Return: no return
+ *
+ */
+
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
