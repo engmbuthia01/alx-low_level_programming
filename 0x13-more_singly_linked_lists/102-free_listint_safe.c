c5cf954b70600b8f7cc66cab04f84681dddf3d50
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-20 02:01:49 -0700

    a function that finds the loop in a linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+
+/**
+ * free_listp2 - frees a linked list
+ * @head: pointer to a pointer of
+ * head of a list.
+ *
+ * Return: void
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *value;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((value = curr) != NULL)
+		{
+			curr = curr->next;
+			free(value);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointere to a pointer of 
+ * head of a list
+ *
+ * Return: size of the new list (free)
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nnodes = 0;
+	listp_t *myptr, *new, *add;
+	listint_t *curr;
+
+	myptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = myptr;
+		myptr = new;
+
+		add = myptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&myptr);
+				return (nnodes);
+			}
+		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
+	}
+
+	*h = NULL;
+	free_listp2(&myptr);
+	return (nnodes);
+}
