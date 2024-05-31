5c38b26bc5c73c46d5c0881e7b5a82a4e4a4b0a6
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-31 02:20:06 -0700

    trailing whitespace

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: num of times the character _ should be printed 
+ * @n: num of times the character _ should be printed
  */
 
 void print_line(int n)
