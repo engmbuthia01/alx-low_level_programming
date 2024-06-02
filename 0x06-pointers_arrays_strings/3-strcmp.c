fd9451cca2a265dfa5b043ead0057e6266460a15
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-06-01 22:54:33 -0700

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,17 @@
  * Return: pointer to the vresulting dest
  */
 
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
-	int index;
+	int i;
+	int value = 0;
 
-	for (index = 0; index < n && src[index] != '\0'; index++)
+	for (i = 0; value == 0; i++)
 	{
-		dest[index] = src[index];
+		if ((s1[i] == '\0') && (s2[i] == '\0'))
+			break;
+		value = s1[i] - s2[i];
 	}
-	while (index < n)
-	{
-		dest[index] = '\0';
-		index++;
-	}
-	return (dest);
+
+	return (value);
 }
